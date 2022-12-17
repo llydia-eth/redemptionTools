5e8d7d329969e6a529a672154edb7f53d7d270d3
Llydia Cross <llydiacross@gmail.com>
2022-12-17 18:29:59 +0000

    s

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { InfinityMintGem, InfinityMintGemParameters } from "@app/interfaces";
+
+const gem: InfinityMintGem = {
+	name: "ERC721 Redemption Tools",
+	deploy: async () => {},
+	init: async (params: InfinityMintGemParameters) => {},
+	setup: async () => {},
+};
+
+export default gem;
